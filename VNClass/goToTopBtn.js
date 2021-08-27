e9d1cb38d7620cf88d222dceb1cf552f88ce0576
RoyEthan <85539985+RoyEthan@users.noreply.github.com>
2021-08-27 15:06:15 +1000

    Add files via upload

--- VNClass/goToTopBtn.js
+++ VNClass/goToTopBtn.js
@@ -17,3 +17,40 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// play only one audio at a time
+
+$(function(){
+    $("audio").on("play", function() {
+        $("audio").not(this).each(function(index, audio) {
+            audio.pause();
+        });
+    });
+});
+
+
+
+// download mp3
+
+function downloadBlob(blob, filename) {
+  var a = document.createElement('a');
+  a.download = filename;
+  a.href = blob;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+}
+
+function downloadResource(url) {
+  filename = url.split('\\').pop().split('/').pop();
+  fetch(url, {
+      mode: 'no-cors'
+    })
+    .then(response => response.blob())
+    .then(blob => {
+      let blobUrl = window.URL.createObjectURL(blob);
+      downloadBlob(blobUrl, filename);
+    })
+    .catch(e => console.error(e));
+}
